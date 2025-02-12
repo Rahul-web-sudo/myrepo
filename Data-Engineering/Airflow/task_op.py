9ebae5dba1caba17f06ae905145e49e2a3b2f57f
Rahul-web-sudo <rahul-garg2@hpe.com>
2025-02-12 12:38:34 +0530

    Create task_op.py

---
+++ Data-Engineering/Airflow/task_op.py
@@ -0,0 +1,55 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+# Define a simple Python function
+def print_hello():
+    print("Hello, World!")
+
+# Define the DAG
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'start_date': datetime(2025, 2, 12),
+}
+
+dag = DAG(
+    'simple_dag_example',
+    default_args=default_args,
+    description='A simple DAG with multiple tasks',
+    schedule_interval=None,  # Run manually for this example
+)
+
+# Define tasks
+start_task = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+task_1 = PythonOperator(
+    task_id='task_1',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+task_2 = PythonOperator(
+    task_id='task_2',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+task_3 = PythonOperator(
+    task_id='task_3',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+end_task = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+# Set task dependencies
+start_task >> task_1 >> task_3 >> end_task
+start_task >> task_2 >> task_3 >> end_task
