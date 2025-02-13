635385e250499ac41117c14a7c8e7094c1059521
Rahul-web-sudo <rahul-garg2@hpe.com>
2025-02-13 19:08:58 +0530

    Update singlep.py

--- Data-Engineering/Airflow/singlep.py
+++ Data-Engineering/Airflow/singlep.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
-from airflow.utils.dates import timedelta
 
 default_args = {
     'owner': 'airflow',
@@ -22,14 +21,13 @@ pod_config = {
     'namespace': 'default',  # The namespace in which the pod will run
     'image': 'python:3.8',    # Example container image (change to the one needed for your tasks)
     'name': 'airflow-task-pod',  # Pod name
-    'task_id': 'run_in_single_pod',  # The task ID
     'is_delete_operator_pod': True,  # Automatically delete the pod after the task is done
     'retries': 1,   # Number of retries in case of failure
 }
 
 # Task 1
 task_1 = KubernetesPodOperator(
-    task_id='task_1',
+    task_id='task_1',  # Explicit task_id here
     cmds=['python', '-c', 'print("Task 1 running")'],
     **pod_config,  # Pass the pod configuration
     dag=dag
@@ -37,7 +35,7 @@ task_1 = KubernetesPodOperator(
 
 # Task 2
 task_2 = KubernetesPodOperator(
-    task_id='task_2',
+    task_id='task_2',  # Explicit task_id here
     cmds=['python', '-c', 'print("Task 2 running")'],
     **pod_config,  # Pass the pod configuration
     dag=dag
@@ -45,7 +43,7 @@ task_2 = KubernetesPodOperator(
 
 # Task 3
 task_3 = KubernetesPodOperator(
-    task_id='task_3',
+    task_id='task_3',  # Explicit task_id here
     cmds=['python', '-c', 'print("Task 3 running")'],
     **pod_config,  # Pass the pod configuration
     dag=dag
